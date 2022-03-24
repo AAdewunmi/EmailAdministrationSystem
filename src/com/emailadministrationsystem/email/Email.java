8514055a6a77373fc3e8be75cca4a688e7b45323
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2022-03-24 11:58:13 +0000

    1st Commit: Skeleton Email Class

---
+++ src/com/emailadministrationsystem/email/Email.java
@@ -0,0 +1,10 @@
+package com.emailadministrationsystem.email;
+
+/**
+ * @author: Adrian Adewunmi
+ * @date:   2022-03-24
+ * This class is used to store the email information.
+ */
+
+public class Email {
+}
